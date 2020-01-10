de5236d239da51b8c9da75db7baab8d0996d3274
FelixIzm <for_cv@mail.ru>
2020-01-10 14:05:09 +0300

    1

--- src/components/Nav.svelte
+++ src/components/Nav.svelte
@@ -55,7 +55,7 @@
 
 		<!-- for the blog link, we're using rel=prefetch so that Sapper prefetches
 		     the blog data when we hover over the link or tap it on a touchscreen -->
-			 <li><a rel=prefetch class:selected='{segment === "blog"}' href='blog'>blog</a></li>
-			 <li><a rel=prefetch class:selected='{segment === "db"}' href='db'>database</a></li>
-			</ul>
+		 <li><a rel=prefetch class:selected='{segment === "blog"}' href='blog'>blog</a></li>
+		 <li><a class:selected='{segment === "db"}' href='db'>database</a></li>
+	</ul>
 </nav>
