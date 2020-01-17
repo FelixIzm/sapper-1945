d5f377296a561e4fbf0e7f2a0cb7b96c0ec8d39c
FelixIzm <for_cv@mail.ru>
2020-01-17 13:23:49 +0300

    fetch

--- src/server.js
+++ src/server.js
@@ -18,13 +18,23 @@ var corsOptions = {
   }
 }
 
+var corsOptionsDelegate = function (req, callback) {
+  var corsOptions;
+  if (whitelist.indexOf(req.header('Origin')) !== -1) {
+    corsOptions = { origin: true } // reflect (enable) the requested origin in the CORS response
+  } else {
+    corsOptions = { origin: false } // disable CORS for this request
+  }
+  callback(null, corsOptions) // callback expects two parameters: error and options
+}
+
 polka() // You can also use Express
 	.use(
 		compression({ threshold: 0 }),
 		sirv('static', { dev }),
 		sapper.middleware()
 	)
-        .get('/items/', cors(corsOptions), function (req, res, next) {
+        .get('/', cors(corsOptionsDelegate), function (req, res, next) {
               res.json({msg: 'This is CORS-enabled for a whitelisted domain.'})
          })
 	.listen(PORT, err => {
