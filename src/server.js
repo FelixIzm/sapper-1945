7afcaea3e9018d73afe64df6eb48f838f0cede84
FelixIzm <for_cv@mail.ru>
2020-01-17 13:58:18 +0300

    server

--- src/server.js
+++ src/server.js
@@ -2,31 +2,10 @@ import sirv from 'sirv';
 import polka from 'polka';
 import compression from 'compression';
 import * as sapper from '@sapper/server';
-var cors = require('cors')
 
 const { PORT, NODE_ENV } = process.env;
 const dev = NODE_ENV === 'development';
 
-var whitelist = ['https://api-1945.herokuapp.com']
-var corsOptions = {
-  origin: function (origin, callback) {
-    if (whitelist.indexOf(origin) !== -1 || !origin) {
-      callback(null, true)
-    } else {
-      callback(new Error('Not allowed by CORS'))
-    }
-  }
-}
-
-var corsOptionsDelegate = function (req, callback) {
-  var corsOptions;
-  if (whitelist.indexOf(req.header('Origin')) !== -1) {
-    corsOptions = { origin: true } // reflect (enable) the requested origin in the CORS response
-  } else {
-    corsOptions = { origin: false } // disable CORS for this request
-  }
-  callback(null, corsOptions) // callback expects two parameters: error and options
-}
 
 polka() // You can also use Express
 	.use(
@@ -34,9 +13,6 @@ polka() // You can also use Express
 		sirv('static', { dev }),
 		sapper.middleware()
 	)
-        .get('https://api-1945.herokuapp.com/', cors(corsOptionsDelegate), function (req, res, next) {
-              res.json({msg: 'This is CORS-enabled for a whitelisted domain.'})
-         })
 	.listen(PORT, err => {
 		if (err) console.log('error', err);
 	});
