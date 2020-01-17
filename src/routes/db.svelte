9f4e88c2feeedbbafa300d1fedf173414823e763
FelixIzm <for_cv@mail.ru>
2020-01-17 10:40:20 +0300

    fetch

--- src/routes/db.svelte
+++ src/routes/db.svelte
@@ -1,7 +1,13 @@
+<script>
+  import Fetch from "./Fetch.svelte";
+</script>
 <svelte:head>
 	<title>DataBase</title>
 </svelte:head>
 
+
+
 <h1>DataBase this site</h1>
 
 <p>DataBase this.</p>
+<Fetch />
