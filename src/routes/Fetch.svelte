2c0735163963ddbad3e079a6cdc45cac5e8d44d2
FelixIzm <for_cv@mail.ru>
2020-01-17 17:01:47 +0300

    sapper

--- src/routes/Fetch.svelte
+++ src/routes/Fetch.svelte
@@ -3,7 +3,7 @@
   let data = [];
   onMount(async function() {
     //const response = await fetch("https://cors-anywhere.herokuapp.com/https://api-1945.herokuapp.com/");
-    const response = await fetch("https://api-1945.herokuapp.com/");
+    const response = await fetch("https://fastify-1945.herokuapp.com/users/fdgdfgdf");
     const json = await response.json();
     data = json;
     console.log(data);
