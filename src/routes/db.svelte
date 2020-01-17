4d4b4992f041ba3e750c61acddf4ab768ae69182
FelixIzm <for_cv@mail.ru>
2020-01-17 17:51:37 +0300

    adf

--- src/routes/db.svelte
+++ src/routes/db.svelte
@@ -2,6 +2,13 @@
 	import Info from './Info_FX.svelte';
 	import { onMount } from "svelte";
 
+  let data = [];
+  onMount(async function() {
+    //const response = await fetch("https://cors-anywhere.herokuapp.com/https://api-1945.herokuapp.com/");
+    const response = await fetch("https://fastify-1945.herokuapp.com/users/fdgdfgdf");
+    const json = await response.json();
+    data = json;
+
 	const pkg = {
 		name: 'svelte',
 		version: 3,
